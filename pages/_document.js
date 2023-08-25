80ee7fc062b8f456c9df9ff53a617f7fbb722f2a
rudygnve <131585190+rudygnve@users.noreply.github.com>
2023-08-25 11:31:50 +0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -47,7 +47,24 @@ export default function Document() {
         `,
         }}
       />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5KTVL64D');
+        `,
+        }}
+      />
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5KTVL64D"className="hidden"></iframe>`,
+        }}
+        ></noscript>
         <Main />
         <NextScript />
       </body>
