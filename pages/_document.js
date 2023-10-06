846d5ae91c37df62106a24bda2ada30c150b415d
rudygnve <131585190+rudygnve@users.noreply.github.com>
2023-10-06 21:19:41 +0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { Analytics } from "@vercel/analytics/react";
+import { SITEURI, SITENAME } from "@/data";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <title>TikRapid - Free TikTok Video Downloader Without Watermark</title>
+      <meta
+        name="description"
+        content={`${SITENAME} is a free TikTok Video Downloader without watermark website. It allows users to download their favorite TikTok videos without any watermark or logo.`}
+      />
       <script
         src="https://t.usermaven.com/lib.js"
         data-key="UMNBwQv7GV"
@@ -21,6 +26,24 @@ export default function Document() {
         }}
       ></script>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6300896650470274" crossorigin="anonymous"></script>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: `
+        {
+          "@context": "https://schema.org/",
+          "@type": "WebSite",
+          "name": "${SITENAME}",
+          "url": "${SITEURI}/",
+          "potentialAction": {
+            "@type": "SearchAction",
+            "target": "{search_term_string}",
+            "query-input": "required name=search_term_string"
+          }
+        }
+        `,
+        }}
+      ></script>
       <body>
         <Main />
         <NextScript />
